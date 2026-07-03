b3cd9c54e0244030f09cc36041a798e674c95e3d
Zoe <83668266+KRavie403@users.noreply.github.com>
2026-07-03 16:21:24 +0900

    feat: ItemDataBase 및 필드 아이템 스폰 시스템 추가
    - 아이템 데이터베이스 관리용 ItemDataBase 싱글톤 추가
    - 아이템 리스트(itemDB) 기반 데이터 보관 구조 구현
    - 필드 아이템 프리팹 스폰 기능 추가
    - Start에서 테스트용 fieldItemPrefab 자동 생성 로직 추가

---
+++ Assets/1. Script/Inventory/ItemDataBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDataBase : MonoBehaviour
+{
+    public static ItemDataBase instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public List<Item> itemDB = new List<Item>();
+
+    public GameObject fieldItemPrefab;
+    public Vector3 pos;
+
+    private void Start()
+    {
+        Instantiate(fieldItemPrefab, pos, Quaternion.identity);
+    }
+}
